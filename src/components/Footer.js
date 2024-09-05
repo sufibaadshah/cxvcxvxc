b1e2606af259f95f0c159a1d5157ed28d3b32686
sufibaadshah <sufyanansari0060@gmail.com>
2024-09-05 21:18:51 +0500

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -17,7 +17,7 @@ function Footer() {
           <h3>Designed and Developed by sufi baadshah</h3>
         </Col>
         <Col md="4" className="footer-copywright">
-          <h3>Copyright © {year} SB</h3>
+          <h3>Copyright © 2024 SB</h3>
         </Col>
         <Col md="4" className="footer-body">
           <ul className="footer-icons">
